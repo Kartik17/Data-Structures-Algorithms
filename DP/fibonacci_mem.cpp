703f9478c196546a017002e7ecedcdf1c4f37886
Kartik17 <kartiksah7@gmail.com>
2020-01-10 16:06:05 -0500

    DP

--- DP/fibonacci_mem.cpp
+++ DP/fibonacci_mem.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int fib(int a,int dp[]){
 	if(a == 0 or a == 1){
 		dp[a] = a;
+		return dp[a];
 	}
-	else{
-		dp[a] = fib(a-1,dp) + fib(a-2,dp);
+	else if(dp[a] != -1){
+		return dp[a];
 	}
-
+	dp[a] = fib(a-1,dp) + fib(a-2,dp);
 	return dp[a];
 }
 
@@ -18,7 +19,7 @@ int main(int argc, char const *argv[])
 
 	int n = 10;
 	static int* dp = new int[n+1];
-	for(int i = 0 ;i<n;i++){
+	for(int i = 0 ;i<=n;i++){
 		dp[i] = -1;
 	}
 	
